9c1b73826225833e36105687d3105a99ef73eb99
Anna Azizova <annaazizova9@gmail.com>
2019-02-02 14:30:42 +0300

    initial commit

---
+++ src/main/java/com/aazizova/springboottesttask/Application.java
@@ -0,0 +1,14 @@
+package com.aazizova.springboottesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Anna on 02.02.2019.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
