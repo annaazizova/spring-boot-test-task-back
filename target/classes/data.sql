c62734d893cc4297180c100950b8d167c5b8bef7
Anna Azizova <annaazizova9@gmail.com>
2019-05-29 01:20:16 +0300

    removed target

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO products (id, name, brand, price, quantity) VALUES (0, 'ProductName0', 'ProductBrand0', 0, 0);
-INSERT INTO products (id, name, brand, price, quantity) VALUES (1, 'ProductName1', 'ProductBrand1', 1, 1);
-INSERT INTO products (id, name, brand, price, quantity) VALUES (2, 'ProductName2', 'ProductBrand2', 2, 2);
-INSERT INTO products (id, name, brand, price, quantity) VALUES (3, 'ProductName3', 'ProductBrand3', 3, 3);
-INSERT INTO products (id, name, brand, price, quantity) VALUES (4, 'ProductName4', 'ProductBrand4', 4, 4);
